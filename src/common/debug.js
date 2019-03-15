01849425fbf69261fff60e88efc49e2ac183fae5
Michael Resnick <biglowresnick@rcn.com>
2019-03-15 11:17:39 -0400

    Have debug.now default to today.

--- src/common/debug.js
+++ src/common/debug.js
@@ -4,21 +4,29 @@
  * @member {Object} debug
  * @property {?boolean} showtimeFilterOff - If falsey , only showtimes after now will be
  *           inclouded in the results, otherewise all showtimes for a film will be incuded.
- * @property {?number[]} nowVals - The six values passed to {@link Date}() and
+ * @property {?Object} nowVals - The six values passed to {@link Date}() and
  *           {@link Date} constructors.
+ * @property {number[]} nowVals.date - Array of [year, month, date]
+ * @property {number[]} nowVals.time - Array of [hours, minutes, seconds, milliseconds]
  * @property {?Date} now - The value created by using the {@link nowVals} property.
- * @property {?Date} requestedDate - If set, {@link Context.requestedDate} will use it, rather than
- *           the value normally used.
  */
 // Comment out what is not to be used.
 const debug = {
   // showtimeFilterOff: true,
-  // nowVals: [2019, 2, 2, 0, 0, 0, 0],
+  nowVals: {
+    // date: [2019, 2, 11],
+    time: [22, 0, 0, 0],
+  },
   // autoFillLists: true,
-  // requestedDate: new Date(2019, 2, 2),
 };
 if (debug.nowVals) {
-  debug.now = new Date(...debug.nowVals);
+  let nowDate = debug.nowVals.date;
+  if (!nowDate) {
+    const now = new Date();
+    nowDate = [now.getFullYear(), now.getMonth(), now.getDate()];
+  }
+  [].concat();
+  debug.now = new Date(...nowDate.concat(debug.nowVals.time));
 }
 
 export default debug;
