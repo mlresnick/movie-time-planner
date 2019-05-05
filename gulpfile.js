88a6881bfc24e72d3c737ab476869d8149eec083
Michael Resnick <biglowresnick@rcn.com>
2019-05-05 17:26:29 -0400

    Minor cleanup; add watch calls

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,17 @@ import del from 'del';
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 
-const { dest, src, parallel, series } = gulp; // eslint-disable-line object-curly-newline
+const { dest, src, parallel, series, watch } = gulp; // eslint-disable-line object-curly-newline
 
 function createLib(cb) {
   src('lib/**/*.*').pipe(dest('dist/lib'));
-  // [
-  //   'framework7/js',
-  //   'framework7/css',
-  //   'framework7-icons/fonts',
-  //   'material-design-icons/iconfont',
-  // ].forEach(dir => src(`node_modules/${dir}/*.*`).pipe(dest(`dist/lib/${dir}/`)));
 
   cb();
 }
 
-// TODO
 function generateDocumentation(cb) {
+  // TODO
+
   cb();
 }
 
@@ -42,5 +37,10 @@ function copySources(cb) {
 const createDist = parallel(copySources, createLib, generateDocumentation, generateCSS);
 
 const recreateDist = series(cleanDist, createDist);
+
+watch(['src/*.*', 'src/js/**/*.*'], parallel(copySources, generateDocumentation));
+watch(['src/scss/**/*.scss', 'src/scss/**/*.css'], generateCSS);
+watch('lib/**/*.*', createLib);
+
 // TODO update taks based on watch results
 export default recreateDist;
